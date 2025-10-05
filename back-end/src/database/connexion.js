0fbbc22c82bbb6f294efe17a94aca053c5a97169
SprigganTwelve <angepaterne.dali@gmail.com>
2025-10-05 02:50:08 +0200

    backend, bdd ssl connection managed

--- back-end/src/database/connexion.js
+++ back-end/src/database/connexion.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
+
 const db = require("mariadb")
+const isProd = process.env.NODE_ENV === "production"
+
 
 const connexion = db.createConnection({
     port     : process.env.DB_PORT,
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
+    ...(isProd ? { ssl: { rejectUnauthorized: true } } : {})
 })
 
 

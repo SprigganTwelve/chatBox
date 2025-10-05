afd48dffdba8219c99ea48bb22bff3d1f6ae00c0
SprigganTwelve <angepaterne.dali@gmail.com>
2025-10-05 03:01:49 +0200

    .

--- back-end/src/database/connexion.js
+++ back-end/src/database/connexion.js
@@ -4,14 +4,20 @@ const db = require("mariadb")
 const isProd = process.env.NODE_ENV === "production"
 
 
-const connexion = db.createConnection({
-    port     : process.env.DB_PORT,
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
-    ...(isProd ? { ssl: { rejectUnauthorized: true } } : {})
-})
 
+const getConnection = async ()=>{
+    const pool = db.createPool({
+        port     : process.env.DB_PORT,
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_NAME,
+        ...(isProd ? { ssl: { rejectUnauthorized: true } } : {})
+    })
+    const conn = await pool.getConnection()
+    return conn
+}
+
+const connexion = getConnection()
 
 module.exports = { connexion };
